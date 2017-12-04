1e6f35ddab290ec218642b94cff9ba3215205e54
note-5i\shurik <abashev.alexandr@gmail.com>
2017-12-04 20:38:45 +0200

    finish

--- src/com/shurik/company/Factorial.java
+++ src/com/shurik/company/Factorial.java
@@ -1,5 +1,20 @@
 package com.shurik.company;
 
 public class Factorial {
-    
+
+    public static int getFactorial(int num) throws FactorialException {
+
+        int result = 1;
+
+        if (num < 1) {
+            throw new FactorialException("Число не может быть меньше 1", num);
+        }
+
+        for (int i = 1; i <= num; i++){
+            result *= i;
+        }
+
+        return result;
+
+    }
 }
--- src/com/shurik/company/Main.java
+++ src/com/shurik/company/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        try {
+
+            int result  = Factorial.getFactorial(0);
+            System.out.println("result -> " + result);
+
+        } catch (FactorialException fe){
+            System.out.println(fe.getMessage());
+            System.out.println(fe.getNumber());
+        }
+
     }
 }
